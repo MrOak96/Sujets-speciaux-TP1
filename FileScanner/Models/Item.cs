43364285fcd1e1b5e593487b9fd59a16fb87f6fc
T3CHNOG4B <gabdeschesnes@gmail.com>
2020-09-15 19:58:06 -0400

    Final

---
+++ FileScanner/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileScanner.Models
+{
+    public class Item
+    {
+        public string Path { get; set; }
+        public string Image { get; set; }
+
+        public Item(string path, string image)
+        {
+            Path = path;
+            Image = image;
+        }
+
+    }
+}
--- FileScanner/ViewModels/MainViewModel.cs
+++ FileScanner/ViewModels/MainViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using FileScanner.Commands;
+using FileScanner.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +15,17 @@ namespace FileScanner.ViewModels
     public class MainViewModel : BaseViewModel
     {
         private string selectedFolder;
-        private ObservableCollection<string> folderItems = new ObservableCollection<string>();
-         
+        private ObservableCollection<Item> items = new ObservableCollection<Item>();
+
         public DelegateCommand<string> OpenFolderCommand { get; private set; }
         public DelegateCommand<string> ScanFolderCommand { get; private set; }
 
-        public ObservableCollection<string> FolderItems { 
-            get => folderItems;
-            set 
-            { 
-                folderItems = value;
+        public ObservableCollection<Item> Items
+        {
+            get => items;
+            set
+            {
+                items = value;
                 OnPropertyChanged();
             }
         }
@@ -41,7 +44,7 @@ namespace FileScanner.ViewModels
         public MainViewModel()
         {
             OpenFolderCommand = new DelegateCommand<string>(OpenFolder);
-            ScanFolderCommand = new DelegateCommand<string>(ScanFolder, CanExecuteScanFolder);
+            ScanFolderCommand = new DelegateCommand<string>(ScanFolderAsync, CanExecuteScanFolder);
         }
 
         private bool CanExecuteScanFolder(string obj)
@@ -60,28 +63,60 @@ namespace FileScanner.ViewModels
             }
         }
 
-        private void ScanFolder(string dir)
+        private async void ScanFolderAsync(string dir)
         {
-            FolderItems = new ObservableCollection<string>(GetDirs(dir));
-            
-            foreach (var item in Directory.EnumerateFiles(dir, "*"))
-            {
-                FolderItems.Add(item);
-            }
+            Items = new ObservableCollection<Item>();
+
+            var folders = await Task.Run(() => GetDirs(dir));
+            var files = await Task.Run(() => GetFiles(dir));
+
+            if(folders != null)
+                await Task.Run(() => AddItems(folders, "/Images/Folder.png"));
+
+            if(files != null)
+                await Task.Run(() => AddItems(files, "/Images/File.png"));
         }
 
-        IEnumerable<string> GetDirs(string dir)
-        {            
-            foreach (var d in Directory.EnumerateDirectories(dir, "*"))
+        public List<string> GetDirs(string dir)
+        {
+            var folders = new List<string>();
+            try
             {
-                yield return d;
+                foreach (var d in Directory.EnumerateDirectories(dir, "*", SearchOption.AllDirectories))
+                    folders.Add(d);
             }
+            catch (UnauthorizedAccessException)
+            {
+                System.Windows.MessageBox.Show("Directory Access denied !");
+                return null;
+            }
+
+            return folders;
         }
 
-        ///TODO : Tester avec un dossier avec beaucoup de fichier
-        ///TODO : Rendre l'application asynchrone
-        ///TODO : Ajouter un try/catch pour les dossiers sans permission
+        public List<string> GetFiles(string dir)
+        {
+            var files = new List<string>();
+            try
+            {
+                foreach (var f in Directory.EnumerateFiles(dir, "*", SearchOption.AllDirectories))
+                    files.Add(f);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                System.Windows.MessageBox.Show("File Access denied !");
+                return null;
+            }
+            return files;
+        }
 
-
+        public void AddItems(List<string> items, string image)
+        {
+            System.Windows.Application.Current.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, (Action)delegate ()
+            {
+                foreach (var file in items)
+                    Items.Add(new Item(file, image));
+            });
+        }
     }
 }
